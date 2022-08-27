fa930a22f481fd85e26d170ded08662965362f40
Shane Mulligan <mullikine@gmail.com>
2022-08-27 22:52:29 +1200

    27.08.22

---
+++ app/Main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import           Control.Monad     (unless)
+import           EvaluatorExamples
+import           System.IO
+
+main :: IO ()
+main = do
+  input <- read'
+
+  unless (input == ":quit")
+       $ print' (eval' input)
+      >> main
+
+
+read' :: IO String
+read' = putStr "REPL> "
+     >> hFlush stdout
+     >> getLine
+
+
+eval' :: String -> String
+eval' input =
+  input
+  -- Try the following ones from `EvaluatorExamples.hs`:
+  -- capitalizer input
+  -- simpleCalc input
+  -- emojiFinder input
+
+
+print' :: String -> IO ()
+print' = putStrLn
